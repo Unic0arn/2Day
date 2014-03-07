78571dc1ebdd0c335fc033f681dee3a54f8fcf84
androidand <sylvan@kth.se>
2014-03-07 15:02:08 +0100

    Gridster Example
    Playing with gridster

--- js/app.js
+++ js/app.js
@@ -5,4 +5,19 @@ $(function() {
 	//We instantiate our model
 	var model = new MainModel();
 
+
+
+
+	//This part is a test for Gridster
+	var gridster;
+
+      $(function() {
+          gridtster = $(".gridster > ul").gridster({
+              widget_margins: [10, 10],
+              widget_base_dimensions: [140, 140],
+              min_cols: 6
+          }).data('gridster');
+      });
+
+
 });	
