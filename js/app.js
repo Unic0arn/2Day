3a1d4aed962cba1b76159f1b5d9ae9914f7898db
androidand <sylvan@kth.se>
2014-03-07 17:33:23 +0100

    Some testing with Gridster an picture
    Will work on the controller after this.

--- js/app.js
+++ js/app.js
@@ -8,15 +8,17 @@ $(function() {
 
 
 
-	//This part is a test for Gridster
+	//This part is a test for Gridster. Should move to GridView!
 	var gridster;
 
       $(function() {
           gridtster = $(".gridster > ul").gridster({
+
               widget_margins: [10, 10],
               widget_base_dimensions: [140, 140],
               min_cols: 6
           }).data('gridster');
+
       });
 
 
