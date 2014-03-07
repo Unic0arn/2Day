53d71c98d588b1b3af7b5286b78c036c0d70a2ca
Fredrik Hallberg <fhallb@kth.se>
2014-03-07 01:24:32 +0100

    Init models
    Created the initial model files.
    Todo: Fill them with fields and functions

---
+++ js/models/2DayModel.js
@@ -0,0 +1,11 @@
+//2Day model constructor
+var 2DayModel = function(){
+	var activities = [];
+	var days = [];
+
+	//
+	// days['2014-03-05'] = new Day();
+
+
+
+}
---
+++ js/models/Day.js
@@ -0,0 +1,5 @@
+var Day = function(){
+	
+
+	
+}
