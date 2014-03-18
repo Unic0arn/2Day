4dd8af2514cf97abd93b08ad0aeb34f8f957fc47
Fredrik Hallberg <fhallb@csc.kth.se>
2014-03-18 16:30:08 +0100

    Fixed overlay and scrollbar

--- js/controllers/dayViewController.js
+++ js/controllers/dayViewController.js
@@ -3,7 +3,7 @@ var DayViewController = function(view, main, model){
 	var initDay = function(_day){
 		model.chooseDay(_day);
 		var day = model.getDay();
-		view.displayDay(day);
+		
 		console.log("Earliest: ") ;
 		console.log(day.getStart());
 		//ReferenceError: invalid assignment left-hand side
@@ -30,34 +30,38 @@ var DayViewController = function(view, main, model){
     	var dayend  = day.getEnd();
     	var daylength = dayend - daystart;
     	var farday = time - daystart;
-    	var maxWidth = view.getScheduleContainerWidth();
+    	var maxWidth = view.getScheduleContainer().width();
     	if(time > dayend){
-    		scrollPlace = maxWidth;
+    		overlay = maxWidth;
     	}else if(time < daystart){
-    		scrollPlace = 0;
+    		overlay = 0;
     	}else{
-    		scrollPlace = (maxWidth * farday) / daylength;
+    		overlay = (maxWidth * farday) / daylength;
     	}
-/*
-    	console.log(scrollPlace);
-    	console.log("Start: " + getTime(daystart) + " End: " + getTime(dayend) + " Length of day: " + daylength);
-    	console.log("Current time: " + getTime(time) + " which is " + farday + "ns since daystart" + ", widh of container is: " + maxWidth);
-    	
-    	*/
-    	
+/*		console.log(scrollPlace);
+    	console.log("Start: " + getTimeFormatted(daystart) + " End: " + getTimeFormatted(dayend) + " Length of day: " + daylength);
+    	console.log("Current time: " + getTimeFormatted(time) + " which is " + farday + "ns since daystart" + ", widh of container is: " + maxWidth);*/
+    	scrollPlace = findScrollPlace(day,view,time);
     	view.updateScroll(scrollPlace);
-    }, 100);
-
-
-
-
+    	view.updateOverlay(overlay);
+    }, 10);
 	this.update = function(arg){
 		console.log(model.getDay());
 		view.displayDay(model.getDay());
 	}
 }
-
-function getTime(date){
+function findScrollPlace(day, view,time){
+	var inner = view.getScheduleContainer();
+	var outer = inner.parent();
+	var scrollArea = (inner.width() - outer.width())
+    var daystart = day.getStart();
+    var dayend  = day.getEnd();
+    var daylength = dayend - daystart;
+    var tar = (time - daystart);
+	var target  = (tar / daylength) * scrollArea;	
+	return target;
+}
+function getTimeFormatted(date){
 	var h = ( date.getHours() < 10 ? "0" : "" ) + date.getHours();
 	var m = ( date.getMinutes() < 10 ? "0" : "" ) + date.getMinutes();
 	var s = ( date.getSeconds() < 10 ? "0" : "" ) + date.getSeconds();
--- js/views/dayView.js
+++ js/views/dayView.js
@@ -34,7 +34,7 @@ var DayView = function(container){
 		 	img.attr('src', "images/" + activity.image);
 			img.addClass("dayImage");
 
-        	divActivity.append(getTime(activity.startTime))
+        	divActivity.append(getTimeFormatted(activity.startTime))
 			divActivity.append(img);
 			divActivity.width(activity.duration * pxPerMin); 
 
@@ -59,11 +59,19 @@ var DayView = function(container){
         container.append(row.clone().append(divSchedule));
         divSchedule.append(divScheduleContainer);
 
-        
+/*        
 			var activityMargins =  $(".activity").length * (parseInt($(".activity").css('margin-left')) + parseInt($(".activity").css('margin-right')));
 			var breakMargins =  $(".break").length * (parseInt($(".break").css('margin-left')) + parseInt($(".break").css('margin-right')));
 		var containerWidth = toMins(day.getDayLength()) * pxPerMin + activityMargins + breakMargins; // Minutes * 2
-			$(".divScheduleContainer").width(containerWidth + 20);
+	*/
+	
+	var containerWidth = 0;
+	var contents = $(".divScheduleContainer").find("div");
+	for (var i=0; i < contents.length; i++){
+		 containerWidth = containerWidth + $(contents[i]).outerWidth(true);
+	 }
+	
+	$(".divScheduleContainer").width(containerWidth);
 		
 		var divClock = $(document.createElement('div'));
 		divClock.addClass("col-md-12");
@@ -83,22 +91,26 @@ var DayView = function(container){
 	this.updateScroll = function(target){
 		var currentPos = $('#daySchedule').scrollLeft();
 
-		if(currentPos < (target + 10) && currentPos > (target - 10)){
-			//target - currentPos < 10 || currentPos - target < 10){
-			console.log("Hit")
+		if(currentPos < (target + 1) && currentPos > (target - 1)){
 			$('#daySchedule').scrollLeft(target);
 		}else{
 			//console.log("Current pos: " + currentPos + " Target: " + target + " Diff: " + (target-currentPos));
-			var newPos = currentPos + ((target - currentPos) / 100);
-			//console.log(target-currentPos);
+			if(target - currentPos > 0){
+				var newPos = currentPos + 1;
+			}else{
+				var newPos = currentPos - 1;
+			}
 			$('#daySchedule').scrollLeft(newPos);
-		}
-		//console.log(target);
-		$('#scheduleOverlay').width(target+10);			
+		}	
+	}
+	this.updateOverlay = function(target){
+		$('#scheduleOverlay').width(target);
 	}
 
-	this.getScheduleContainerWidth = function(){
-		return $('.divScheduleContainer').width();
+	this.getScheduleContainer = function(){
+		return $('.divScheduleContainer');
 	}
 
 }
+
+
